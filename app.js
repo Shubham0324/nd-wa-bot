fe4f4668661a39b31ecd6ecea610ab8190e8d375
Shubham Aggarwal <shubham2403tayal@gmail.com>
2023-07-31 13:27:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 
 app.post("/api/v1/bot", (req, res) => {
-  const request = req.body["postData"]["Content"];
-  const jsonData = JSON.parse(request);
+  // const request = req.body["postData"]["Content"];
+  // const jsonData = JSON.parse(request);
 
-  const phoneNumber = jsonData.user.id;
-  const name = jsonData.user.name;
-  const textName = jsonData.message.text;
-  const fromMe = jsonData.message.fromMe;
-  const id = jsonData.message.id;
+  // const phoneNumber = jsonData.user.id;
+  // const name = jsonData.user.name;
+  // const textName = jsonData.message.text;
+  // const fromMe = jsonData.message.fromMe;
+  // const id = jsonData.message.id;
 
-  console.log(phoneNumber);
-  console.log(name);
-  console.log(textName);
-  console.log(fromMe);
-  console.log(id);
+  // console.log(phoneNumber);
+  // console.log(name);
+  // console.log(textName);
+  // console.log(fromMe);
+  // console.log(id);
+
+  console.log(req.body);
 });
 
 app.listen(3000, () => {
